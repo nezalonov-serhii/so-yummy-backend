f2798aef569a253b5c6d97818630bbfef3fede66
OlehBrov <o.dudnyk85@gmail.com>
2023-07-14 18:42:15 +0300

    swagger-popular route

--- routes/api/popularRoutes.js
+++ routes/api/popularRoutes.js
@@ -4,6 +4,36 @@ const authenticate = require("../../middlewares/authenticate");
 const popularController = require("../../controllers/popularController");
 
 router.use(authenticate);
+
+/**
+ * @openapi
+ * tags:
+ *  name: Popular
+ *  description: Brings list of popular recipes.
+ */
+
+/**
+ * @openapi
+ *  /popular-recipe:
+ *      get:
+ *          summary: Returns list of 4 most popular recipes
+ *          tags: [Popular]
+ *          responses:
+ *              200:
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  code:
+ *                                      type: integer
+ *                                  message:
+ *                                      type: string
+ *                                  data: 
+ *                                      type: array
+ *                                      items:
+ *                                          $ref: "#/components/schemas/Recipe"
+ */
 router.get("/", popularController.popularRecipes);
 
 module.exports = router;
