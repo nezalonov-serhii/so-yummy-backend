38ed353c4e0f4e8a6ef634be6c324c951e3c70f6
OlehBrov <o.dudnyk85@gmail.com>
2023-07-14 22:53:13 +0300

    fixes w recipe model, better own recipe add

--- controllers/onwRecipesController.js
+++ controllers/onwRecipesController.js
@@ -13,7 +13,6 @@ const getOwnRecipes = async (req, res, next) => {
     populate: {
       path: "_id",
       model: Recipe,
-      populate: [{ path: "ingredients.id", model: Ingredients }],
     },
   });
 
@@ -32,28 +31,27 @@ const postOwnRecipe = async (req, res, next) => {
   const { _id } = req.user;
   const recipe = req.body;
   let uploadRecipeImg = {};
-  // console.log("req file before", req.file);
-  // console.log("recipe", recipe);
+  let temporaryName = "";
+
   if (req.file) {
-    console.log("req file in if", req.file);
-    const { path: temporaryName } = req.file;
+    temporaryName = req.file.path;
     uploadRecipeImg = await uploadRecipeImage(temporaryName);
   }
 
   const newRecipe = await Recipe.create({
     ...recipe,
     thumb: uploadRecipeImg.hasOwnProperty("url") ? uploadRecipeImg.url : "",
-    imgPiblicId: uploadRecipeImg.hasOwnProperty("public_id")
+    imgPublicId: uploadRecipeImg.hasOwnProperty("public_id")
       ? uploadRecipeImg.public_id
       : "",
     owner: _id,
   });
-// console.log('new recipe', newRecipe)
+  // console.log('new recipe', newRecipe)
   await User.findByIdAndUpdate(_id, {
     $push: { ownRecipes: { ...newRecipe } },
   });
 
-    if (req.file) {
+  if (req.file) {
     fs.unlink(temporaryName);
   }
 
@@ -68,7 +66,7 @@ const deleteOwnRecipe = async (req, res, next) => {
   const { id: idToDelete } = req.params;
 
   const deleted = await Recipe.findByIdAndDelete(idToDelete);
-  await deleteRecipeImg(deleted.imgPiblicId);
+  await deleteRecipeImg(deleted.imgPublicId);
   await User.findOneAndUpdate(
     { _id: _id },
     {
