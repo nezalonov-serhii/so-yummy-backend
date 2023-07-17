58ed936e922ffe5a3a99e96e6c3bfd91c6ded0f1
OlehBrov <o.dudnyk85@gmail.com>
2023-07-17 17:04:29 +0300

    fix req.body
    Added new middleware for modifying req.body only on own-recipes POST route

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const authenticate = require("./authenticate");
 const avatarsMiddleware = require("./avatarsMiddleware");
 const validateBody = require("./validateBody");
+const validateRequest = require('./validateRequest');
 
 module.exports = {
   authenticate,
   avatarsMiddleware,
   validateBody,
+  validateRequest,
 };
