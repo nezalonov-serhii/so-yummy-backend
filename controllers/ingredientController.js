8b2b16e748563f60b9908c91dc0888abf1120e49
OlehBrov <o.dudnyk85@gmail.com>
2023-07-17 21:00:40 +0300

    fix error no ingredient

--- controllers/ingredientController.js
+++ controllers/ingredientController.js
@@ -1,50 +1,52 @@
 const Ingredient = require("../models/ingredientsModel");
 const Recipe = require("../models/recipeModel");
 const { ctrlWrapper } = require("../helpers");
+const HttpError = require('../helpers/HttpError')
 
 const ingredientList = async (req, res, next) => {
-
-      const result = await Ingredient.find();
-      res.status(200).json({
-        code: 200,
-        message: "Success",
-        data: result,
-        qty: result.length,
-      });
-}
+  const result = await Ingredient.find();
+  res.status(200).json({
+    code: 200,
+    message: "Success",
+    data: result,
+    qty: result.length,
+  });
+};
 
 const findRecipesByIngredient = async (req, res, next) => {
-    const {query} = req.body;
-    
-    const searchedIngredients = await Ingredient.find({
-      name: { $regex: query, $options: "i" },
-    });
-      if (searchedIngredients.length === 0) {
-        throw HttpError(404, "ingredient not found");
-      }
+  const { query } = req.body;
 
-    const ids = searchedIngredients.map((ingredient) => ingredient.id);
+  const searchedIngredients = await Ingredient.find({
+    name: { $regex: query, $options: "i" },
+  });
+  if (searchedIngredients.length === 0) {
+    throw HttpError(404, "ingredient not found");
+  }
 
-    const result = await Recipe.find({
-        ingredients: {
-            $elemMatch: {
-                id: {
-                    $in: [...ids],
-                }
-            },
+  const ids = searchedIngredients.map((ingredient) => ingredient.id);
+
+  const result = await Recipe.find({
+    ingredients: {
+      $elemMatch: {
+        id: {
+          $in: [...ids],
+        },
       },
-    });
-          if (result.length === 0) {
-            throw HttpError(404, `Recipe with ingredient ${query} is not found`);
-          }
+    },
+  });
+   
+  if (result.length === 0) {
+    
+    throw HttpError(404, `Recipe with ingredient ${query} is not found`);
+  }
 
-          res.status(200).json({
-            code: 200,
-            message: "Success",
-            data: result,
-            qty: result.length,
-          });
-}
+  res.status(200).json({
+    code: 200,
+    message: "Success",
+    data: result,
+    qty: result.length,
+  });
+};
 
 module.exports = {
   ingredientList: ctrlWrapper(ingredientList),
